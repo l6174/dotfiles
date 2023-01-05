06b6b99032edb91847fb1a7754e3583cdf55f529
l6174 <l.lawliet6174@gmail.com>
2023-01-06 04:38:04 +0530

    Fixed Arch Installer

--- install_arch.sh
+++ install_arch.sh
@@ -6,3 +6,10 @@ if [ "$confirmation" = "Y" ] || [ "$confirmation" = "y" ]; then
 else
         exit 1
 fi
+echo -e "This script is divided into 2 parts: Base installation and Setup.\nRun this script 2 times and select which installation you are performing.\nRun base installation after partitioning and Post installation after chrooting into /mnt."
+read -p "Select installation type (B)ase/(P)ost (Default=Base): " itype
+if [ "$itype" = "P" ] || [ "$itype" = "p" ]; then
+        sh .l/script_base.sh
+else
+        sh .l/script_post.sh
+fi
