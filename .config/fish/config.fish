9fc5c45519f5aa88fb38b92f0aa89e1b383ec57e
l6174 <l.lawliet6174@gmail.com>
2023-01-05 09:09:13 +0530

    Added Fish Config

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+fish_add_path --path $HOME/.local/bin
+fish_add_path --path $HOME/bin
+starship init fish | source
+zoxide init --cmd cd fish | source
+set -x BAT_THEME Catppuccin-macchiato
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # neofetch --ascii radioactive.txt --ascii_colors 2 1
+    fetchit -f .l/laptop.txt -o yellow -t red -b blue
+end
+if status --is-login
+    startx    
+    
+end
+fish_add_path /home/llawliet/.spicetify
