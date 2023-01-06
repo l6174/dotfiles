b013f4b1dcfd9460a00c930e52aa994e977dd348
l6174 <l.lawliet6174@gmail.com>
2023-01-06 13:01:57 +0530

    Fixed Errors

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ sleep 3
 echo "Copying files to their place..."
 cp -r ./* ~
 cd ~
+echo "Done!"
+sleep 3
 
 
 # Install Paru
@@ -15,22 +17,27 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 cd ~
+echo "Done!"
+sleep 3
 
 
 # Update
 echo "System Update..."
 sudo pacman --noconfirm -Syu
+echo "Done!"
+sleep 3
 
 
 # Change 'xf86-video-intel' accordingly
-# Remove the things which are bloat to you like chromium, zathura, etc.
 echo "Installing Packages..."
-sudo pacman -S --noconfirm --needed base-devel wget git xf86-video-intel rofi feh xorg xorg-xinit xorg-xinput xmonad fish alsa-utils cava lxappearance qt5ct polkit thunar zoxide exa zathura chromium papirus-icon-theme kitty nitrogen
+sudo pacman -S --no-confirm --needed base-devel wget git xf86-video-intel rofi feh xorg xorg-xinit xorg-xinput xmonad
+echo "Done!"
+sleep 3
 
 
-# AUR Dependencies
+# Extra Packages
 echo "Installing More Packages..."
-paru -S picom-jonaburg-git\
+paru -S --needed picom-jonaburg-git\
 	acpi              \
 	wmctrl            \
 	playerctl         \
@@ -42,7 +49,23 @@ paru -S picom-jonaburg-git\
 	rofi-greenclip    \
 	xautolock         \
 	betterlockscreen  \
-        lightly-qt
+        lightly-qt        \
+        fish              \
+        alsa-utils        \
+        cava              \
+        lxappearance      \
+        qt5ct             \
+        polkit            \
+        thunar            \
+        zoxide            \
+        exa               \
+        zathura           \
+        chromium          \
+        papirus-icon-theme\
+        kitty             \
+        nitrogen
+echo "Done!"
+sleep 3
 
 
 # Quick Fix for tint2
@@ -50,31 +73,53 @@ echo "Applying a quick fix for tint2.."
 if ! [ -f /usr/lib/libasan.so.6 ]; then
     sudo ln -s /usr/lib/libasan.so.8 /usr/lib/libasan.so.6 
 fi
+echo "Done!"
+sleep 3
 
 
 # Change TTY Theme
 echo "Changing TTY Theme..."
 ~/TTY/build.sh
 ~/TTY/install.sh macchiato
+echo "Done!"
+sleep 3
 
 
 # Add Cursors
 echo "Installing Cursors..."
 sudo cp -r ~/.local/share/icons/Catppuccin-Macchiato-Green-Cursors /usr/share/icons/
+echo "Done!"
+sleep 3
 
 
 # Add GTK Theme
 echo "Installing GTK Theme..."
 sudo cp -r ~/.themes/Catppuccin-Macchiato-Standard-Green-Dark /usr/share/themes/
+echo "Done!"
+sleep 3
 
 
 # Set Wallpaper
+echo "Setting Wallpaper..."
 nitrogen --set-auto ~/Wallpapers/Bridge.jpg
+echo "Done!"
+sleep 3
+
+# Set Lockscreen Wallpaper
+echo "Setting Lockscreen Wallpaper..."
+betterlockscreen -u ~/Wallpapers/cat_Japanese_Neon.png --fx blur
+echo "Done!"
+sleep 3
+
+
 # Change Shell
 echo "Changing Shell..."
 chsh -s /usr/bin/fish
+echo "Done!"
+sleep 3
 
 
+# Finalizing
 echo "Installation Complete! Rebooting in 5 seconds..."
 sleep 5
 reboot
