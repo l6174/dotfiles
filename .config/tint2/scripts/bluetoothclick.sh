ecd5ec2c9d7776d8a09a47d1ef55329d2f2c498a
l6174 <l.lawliet6174@gmail.com>
2023-01-11 19:05:28 +0530

    Changed Notify Message

--- .config/tint2/scripts/bluetoothclick.sh
+++ .config/tint2/scripts/bluetoothclick.sh
@@ -1,7 +1,7 @@
 x=$(bluetoothctl info | grep "Alias" | awk '{print $2}')
 if [ "$(bluetoothctl info | grep "Connected" | awk '{print $2}')" = "yes" ]; then
-        dunstify -r 5565 "Connected to $x" -i ~/.local/share/icons/mdi/bluetooth-connected.png
-	exit 0
+        dunstify -r 5565 "Bluetooth" "Connected to $x" -i ~/.local/share/icons/mdi/bluetooth-connected.png
+        exit 0
 else
-        dunstify -r 5565 "Not Connected" -i ~/.local/share/icons/mdi/bluetooth-off.png
+        dunstify -r 5565 "Bluetooth" "Not Connected" -i ~/.local/share/icons/mdi/bluetooth-off.png
 fi
--- .config/tint2/scripts/networkclick.sh
+++ .config/tint2/scripts/networkclick.sh
@@ -3,9 +3,9 @@
 x=$(connmanctl services | awk 'NR==1{print $2}')
 CONNAME=$(connmanctl state | rg State | awk 'NR==1{print $3}')
 if [ "$CONNAME" = "ready" ] || [ "$CONNAME" = "online" ]; then
-    dunstify -r 3456 "Connected to $x" -i ~/.local/share/icons/mdi/wifi.png
+    dunstify -r 3456 "Internet" "Connected to $x" -i ~/.local/share/icons/mdi/wifi.png
     exit 0
 else
-    dunstify -r 3456 "Not Connected" -i ~/.local/share/icons/mdi/wifi-off.png
+    dunstify -r 3456 "Internet" "Not Connected" -i ~/.local/share/icons/mdi/wifi-off.png
     exit 0
 fi
