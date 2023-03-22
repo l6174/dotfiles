0c48a816bbc9bec658b5ef88d73046f5fd8d3632
l6174 <l.lawliet6174@gmail.com>
2023-03-22 08:55:18 +0530

    Replaced iwd with wpa_supplicant

--- .config/tint2/scripts/networkmanager_iwd.sh
+++ .config/tint2/scripts/networkmanager_iwd.sh
@@ -6,19 +6,12 @@
 
 # Dependencies: NetworkManager, curl
 
-    CONNAME=$(nmcli -a | grep 'Wired connection' | awk 'NR==1{print $1}')
-    if [ "$CONNAME" = "" ]; then
-        CONNAME=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -c 5-)
-    fi
-
-    PRIVATE=$(nmcli -a | grep 'inet4 192' | awk '{print $2}')
-    PUBLIC=$(curl -s https://ipinfo.io/ip)
+    CONNAME=$(connmanctl state | rg State | awk 'NR==1{print $3}')
 
     printf "%s" "$SEP1"
-	if [ "$CONNAME" != "" ]; then    
+	if [ "$CONNAME" = "ready" ] || [ "$CONNAME" = "online" ]; then
             printf " %s" # %s" "$CONNAME" ########"$PRIVATE" "$PUBLIC"🌐
         else
 	    printf " %s"
 	fi 
     printf "%s" #"$SEP2"
-
