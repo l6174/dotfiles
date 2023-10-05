3b1f895ad0158828f5e78503d229bce242e4f641
l6174 <l.lawliet6174@gmail.com>
2023-10-06 01:27:22 +0530

    Changed Lock Alert Icon

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xset s 500 &
-xautolock -time 5 -locker "betterlockscreen -l blur" -notify 30 -notifier "notify-send 'Locker' 'Locking screen in 30 seconds'" -killtime 5 -killer "systemctl suspend"
+xautolock -time 5 -locker "betterlockscreen -l blur" -notify 30 -notifier "dunstify 'Locker' 'Locking screen in 30 seconds' -i /home/llawliet/.local/share/icons/mdi/lock-alert.png" -killtime 5 -killer "systemctl suspend"
